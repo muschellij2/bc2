4a0d93519345f1c7fe99e5d6d417d06db875c819
Zhang <zhangh24@nci-02034757-ml.nih.gov>
2017-09-06 10:25:22 -0400

    meta analysis

---
+++ R/LogoddsMetaAnalysis.R
@@ -0,0 +1,21 @@
+#' Title
+#'
+#' @param logodds1
+#' @param info1
+#' @param logodds2
+#' @param info2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+LogoddsMetaAnalysis <- function(logodds1,info1,logodds2,info2){
+  w1 <- solve(info1)
+  w2 <- solve(info2)
+  logodds.meta <- solve(w1+w2)%*%(w1%*%logodd1+w2%*%logodds2)
+  info.meta <- solve(w1+w2)
+
+  return(logodds.meta = logodds.meta,
+         info.meta = info.meta)
+
+}
