05c90acd3e9312d5c6c1fcdd32edbf478c7bf306
Zhang <zhangh24@nci-02034757-ml.nih.gov>
2017-09-07 08:10:05 -0400

    DisplayHeterTest

--- R/display_95CI_p.R
+++ R/display_95CI_p.R
@@ -1,20 +1,32 @@
+
 #' Title
 #'
-#' @param delta_odds
-#' @param delta_test_low_odds
-#' @param delta_test_high_odds
-#' @param p_value
+#' @param logodds
+#' @param sigma
 #'
 #' @return
 #' @export
 #'
 #' @examples
-display_95CI_p = function(delta_odds,delta_test_low_odds,delta_test_high_odds,p_value){
+DisplayTestResult = function(logodds,sigma){
+  var.logodds <- diag(sigma)
+  logodds.low <- logodds-1.96*sqrt(var.logodds)
+  logodds.high <- logodds+1.96*sqrt(var.logodds)
+  odds <- exp(logodds)
+  odds.low <- exp(logodds.low)
+  odds.high <- exp(logodds.high)
+  odds <- format(odds,digits = 2)
+  odds.low <- format(odds.low,digits=2)
+  odds.high <- format(odds.high,digits=2)
+  p.global.assoc <- GlobalTestForAssoc(logodds,sigma)
+  p.global.heter <- GlobalTestForHeter(logodds,sigma)
+  p.individual.heter <- IndividualHeterTest(logodds,sigma)
   result = NULL
-  for(i in 1:length(delta_odds)){
-    result= c(result,paste0(delta_odds[i],"(",delta_test_low_odds[i],"-",
-                            delta_test_high_odds[i],")"),
-              p_value[i])
+  for(i in 1:length(logodds)){
+    result= c(result,paste0(odds[i],"(",odds.low[i],"-",
+                            odds.high[i],")"),
+              p.individual.heter[i])
   }
+  result <- c(result,p.global.assoc, p.global.heter)
   return(result)
 }
