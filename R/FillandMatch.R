c710709367c121f062695a31c45cec0a52285622
Zhang <andrew.haoyu@gmail.com>
2017-11-20 12:25:52 -0500

    update

--- R/FillandMatch.R
+++ R/FillandMatch.R
@@ -8,7 +8,7 @@
 #' @export
 #'
 #' @examples
-FillandSelect <- function(reference,all){
+FillandMatch <- function(reference,all){
   idx.fil <- which(all%in%reference)
   all.fill <- all[idx.fil]
   idx.match <- match(reference,all.fill)
