687767b538a6cf4eb441274a7c97090ba646e058
Zhang <andrew.haoyu@gmail.com>
2017-11-20 12:22:52 -0500

    fill and select

---
+++ R/FillandMatch.R
@@ -0,0 +1,16 @@
+
+#' Title
+#'
+#' @param reference
+#' @param all
+#'
+#' @return
+#' @export
+#'
+#' @examples
+FillandSelect <- function(reference,all){
+  idx.fil <- which(all%in%reference)
+  all.fill <- all[idx.fil]
+  idx.match <- match(reference,all.fill)
+  return(list(idx.fill,idx.match))
+}
