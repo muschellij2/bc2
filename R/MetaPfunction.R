3bc833898557878d3f3a70445a9470992241e36b
Zhang <andrew.haoyu@gmail.com>
2018-11-07 09:49:30 -0500

    update metaodds funtion

--- R/MetaPfunction.R
+++ R/MetaPfunction.R
@@ -7,6 +7,54 @@
 #' @export
 #'
 #' @examples
+Metaoddsfunction <- function(icog_onco_score_infor_one,second.num){
+  score.icog <- as.numeric(icog_onco_score_infor_one[1:(second.num)])
+  infor.icog <- matrix(as.numeric(icog_onco_score_infor_one[(second.num+1):
+                                                              (second.num+second.num^2) ]),
+                       ncol = second.num)
+  start <- second.num+second.num^2
+  score.onco <- as.numeric(icog_onco_score_infor_one[(1+start):
+                                                       (second.num+start)])
+  infor.onco <- matrix(as.numeric(icog_onco_score_infor_one[(second.num+1+start):
+                                                              (second.num+second.num^2+start) ]),ncol=second.num)
+
+
+  meta.result <- LogoddsMetaAnalysis(score.icog,infor.icog,
+                                     score.onco,infor.onco)
+  score.meta <- t(meta.result[[1]])
+  infor.meta <- meta.result[[2]]
+  p.meta <- DisplaySecondStageTestResult(score.meta,infor.meta)[[3]]
+  return(list(log.odds=score.meta,
+              covar = infor.meta,
+              p = p.meta))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' Title
+#'
+#' @param icog_onco_score_infor_one
+#' @param second.num
+#'
+#' @return
+#' @export
+#'
+#' @examples
 MetaPfunction <- function(icog_onco_score_infor_one,second.num){
   score.icog <- as.numeric(icog_onco_score_infor_one[1:(second.num)])
   infor.icog <- matrix(as.numeric(icog_onco_score_infor_one[(second.num+1):
@@ -19,7 +67,7 @@ MetaPfunction <- function(icog_onco_score_infor_one,second.num){
                                                               (second.num+second.num^2+start) ]),ncol=second.num)
 
 
-  meta.result <- ScoreMetaAnalysis(score.icog,infor.icog,
+  meta.result <- LogoddsMetaAnalysis(score.icog,infor.icog,
                                    score.onco,infor.onco)
   score.meta <- t(meta.result[[1]])
   infor.meta <- meta.result[[2]]
--- R/ZSelfDesigntoZallnew.R
+++ R/ZSelfDesigntoZallnew.R
@@ -2,11 +2,13 @@
 #' Title
 #'
 #' @param x.self.design
+#' @param x.self.design2
 #' @param baselineonly
 #' @param additive
 #' @param pairwise.interaction
 #' @param saturated
 #' @param z.design
+#' @param z.design2
 #' @param z.design.baselineonly
 #' @param z.design.additive
 #' @param z.design.pairwise.interaction
@@ -16,12 +18,14 @@
 #' @export
 #'
 #' @examples
-ZSelfDesigntoZall <- function(x.self.design,
+ZSelfDesigntoZallnew <- function(x.self.design,
+                              x.self.design2,
                               baselineonly,
                               additive,
                               pairwise.interaction,
                               saturated,
                               z.design,
+                              z.design2,
                               z.design.baselineonly,
                               z.design.additive,
                               z.design.pairwise.interaction,
@@ -31,25 +35,31 @@ ZSelfDesigntoZall <- function(x.self.design,
   M <- nrow(z.design.additive)
   ##the number of covariates in different potential model structures
   self.design.number <- CountCovarNumber(x.self.design)
+  self.design.number2 <- CountCovarNumber(x.self.design2)
   baselineonly.number <- CountCovarNumber(baselineonly)
   additive.number <- CountCovarNumber(additive)
   pairwise.interaction.number <- CountCovarNumber(pairwise.interaction)
   saturated.number <- CountCovarNumber(saturated)
   ###second.stage.category for different model structures
   self.design.second.cat <- ncol(z.design)
+  self.design.second.cat2 <- ncol(z.design2)
   baselineonly.second.cat <- 1
   additive.second.cat <- ncol(z.design.additive)
   pairwise.interaction.second.cat <- ncol(z.design.pairwise.interaction)
   saturated.second.cat <- ncol(z.design.saturated)
   ###1 for intercept
-  total.covar.number <- 1+ self.design.number+baselineonly.number+additive.number+
+  total.covar.number <- 1+ self.design.number+self.design.number2+
+    baselineonly.number+additive.number+
     pairwise.interaction.number+saturated.number
 
-  z.all <- matrix(0,nrow=(M*total.covar.number),ncol = (M+self.design.second.cat*self.design.number+baselineonly.number*baselineonly.second.cat+
+  z.all <- matrix(0,nrow=(M*total.covar.number),ncol = (M+self.design.second.cat*self.design.number+
+                                                          self.design.second.cat2*self.design.number2+                baselineonly.number*baselineonly.second.cat+
                                                           additive.second.cat*additive.number+
                                                           pairwise.interaction.second.cat*pairwise.interaction.number)+saturated.second.cat*saturated.number)
 
-  for(i in c("intercept","self design","baselineonly",
+  for(i in c("intercept","self design",
+             "self design2",
+             "baselineonly",
              "additive","pairwise.interaction",
              "satuared")){
     ##we always keep intercept as saturated model and to simply, we always use diagnonal matrix for intercept
@@ -72,9 +82,22 @@ ZSelfDesigntoZall <- function(x.self.design,
           }
         }
       }
-    }else if(i=="baselineonly"){
-      column.start = M+self.design.number*self.design.second.cat
+    }else if(i=="self design2"){
+      column.start <- M+self.design.number*self.design.second.cat
       row.start <- 1+self.design.number
+      if(self.design.number2!=0){
+        for(j in 1:M){
+          for(k in 1:self.design.number2){
+            z.all[row.start+k+(j-1)*total.covar.number,
+                  (column.start+(k-1)*self.design.second.cat2+1):
+                    (column.start+k*self.design.second.cat2)] <- as.matrix(z.design[j,])
+          }
+        }
+      }
+    }else if(i=="baselineonly"){
+      column.start = M+self.design.number*self.design.second.cat+self.design.number2*self.design.second.cat2
+      row.start <- 1+self.design.number+
+        self.design.number2
       ###test whether there is any baselineonly variable
       if(baselineonly.number!=0){
         for(j in 1:M){
@@ -86,8 +109,8 @@ ZSelfDesigntoZall <- function(x.self.design,
         }
       }
     }else if(i=="additive"){
-      column.start <- M+self.design.number*self.design.second.cat+baselineonly.number
-      row.start <- 1+self.design.number+baselineonly.number
+      column.start <- M+self.design.number*self.design.second.cat+self.design.number2*self.design.second.cat2+baselineonly.number
+      row.start <- 1+self.design.number+ self.design.number2+baselineonly.number
       if(additive.number!=0){
         for(j in 1:M){
           for(k in 1:additive.number){
@@ -98,8 +121,8 @@ ZSelfDesigntoZall <- function(x.self.design,
         }
       }
     }else if(i == "pairwise.interaction"){
-      column.start <- M+self.design.number*self.design.second.cat+baselineonly.number+additive.number*additive.second.cat
-      row.start <- 1+self.design.number+baselineonly.number+additive.number
+      column.start <- M+self.design.number*self.design.second.cat+self.design.number2*self.design.second.cat2+baselineonly.number+additive.number*additive.second.cat
+      row.start <- 1+self.design.number+self.design.number2+baselineonly.number+additive.number
       if(pairwise.interaction.number!=0){
         for(j in 1:M){
           for(k in 1:pairwise.interaction.number){
@@ -110,9 +133,9 @@ ZSelfDesigntoZall <- function(x.self.design,
         }
       }
     }else {
-      column.start <- M+self.design.number*self.design.second.cat+baselineonly.number+additive.number*additive.second.cat+
+      column.start <- M+self.design.number*self.design.second.cat+self.design.number2*self.design.second.cat2+baselineonly.number+additive.number*additive.second.cat+
         pairwise.interaction.number*pairwise.interaction.second.cat
-      row.start <- 1+self.design.number+baselineonly.number+additive.number+pairwise.interaction.number
+      row.start <- 1+self.design.number+self.design.number2+baselineonly.number+additive.number+pairwise.interaction.number
       if(saturated.number!=0){
         for(j in 1:M){
           for(k in 1:saturated.number){
